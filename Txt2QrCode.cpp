d8d7f858772715acefc493c1deab7cddd914ddee
Bartiszny <29127947+Bartiszny@users.noreply.github.com>
2020-10-25 19:55:36 +0100

    Add all files

---
+++ Txt2QrCode.cpp
@@ -0,0 +1,202 @@
+/* 
+ * QR Code generator demo (C++)
+ * 
+ * Run this command-line program with no arguments. The program computes a bunch of demonstration
+ * QR Codes and prints them to the console. Also, the SVG code for one QR Code is printed as a sample.
+ * 
+ * Copyright (c) Project Nayuki. (MIT License)
+ * https://www.nayuki.io/page/qr-code-generator-library
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+#include <windows.h>
+#include <cstdint>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "QrCode.hpp"
+#include "SDL2/SDL.h"
+using std::uint8_t;
+using qrcodegen::QrCode;
+using qrcodegen::QrSegment;
+const int WINDOW_W = 640;
+const int WINDOW_H = 480;
+// Function prototypes
+static void doBasicDemo(SDL_Window *window, SDL_Surface *surface);
+static void printQr(const QrCode &qr);
+static void printQr1(const QrCode &qr);
+static void printQr2(const QrCode &qr);
+static void printQr3(const QrCode &qr, const int border, SDL_Surface * target);
+// The main application program.
+#undef main
+int main() {
+
+/*---- SDL Init ----*/
+SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window *window = SDL_CreateWindow("gradient",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		WINDOW_W, WINDOW_H, 0);
+		
+	SDL_Surface *surface = SDL_GetWindowSurface(window);
+	doBasicDemo(window, surface);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
+}
+/*---- Demo suite ----*/
+
+// Creates a single QR Code, then prints it to the console.
+static void doBasicDemo(SDL_Window * window, SDL_Surface *surface) {
+	const char *text = "https://google.com";              // User-supplied text
+	const QrCode::Ecc errCorLvl = QrCode::Ecc::LOW;  // Error correction level
+	
+	// Make and print the QR Code symbol
+	const QrCode qr = QrCode::encodeText(text, errCorLvl);
+	//printQr(qr);
+	printQr1(qr);
+	//printQr2(qr);
+	std::cout << qr.toSvgString(4) << std::endl;
+	std::ofstream f("code.svg");
+	if(f) f << qr.toSvgString(4);
+	
+	const int W = qr.getSize()+8;
+    const int H = qr.getSize()+8;
+	const int W2 = 256;
+    const int H2 = 256;
+	static SDL_Surface *gradient = SDL_CreateRGBSurface(
+		0, W, H, 24, 0xff << 0, 0xff << 8, 0xff << 16, 0);
+	
+	
+	printQr3(qr, 4, gradient);
+		SDL_Surface *temp = SDL_CreateRGBSurface(
+	0, W2, H, 24 ,0xff << 0, 0xff << 8, 0xff << 16, 0);
+	
+	uint8_t *pixels = static_cast<uint8_t*>(gradient->pixels);
+	uint8_t *pixels2 = static_cast<uint8_t*>(temp->pixels);
+	int multipler = H2/H;
+	int k = 0;
+	for (short i = 0; i<H; i++) {
+		k = 0;
+		for (short j = 0; j < W; j++) {
+			for (short l = 1; l<=multipler; l++) {			
+			pixels2[k * 3 + i * temp->pitch + 0] = pixels[j * 3 + i * gradient->pitch + 0];
+			pixels2[k * 3 + i * temp->pitch + 1] = pixels[j * 3 + i * gradient->pitch + 1];
+			pixels2[k * 3 + i * temp->pitch + 2] = pixels[j * 3 + i * gradient->pitch + 2]; 
+			k++;
+			}
+		}		
+	}
+	
+	SDL_Surface *target = SDL_CreateRGBSurface(
+	0, W2, H2, 24 ,0xff << 0, 0xff << 8, 0xff << 16, 0);
+	uint8_t *pixels3 = static_cast<uint8_t*>(target->pixels);
+	
+	for (short i = 0; i<multipler*H; i++) {
+		k = 0;
+		for (short j = 0; j < W; j++) {
+			for (short l = 1; l<=multipler; l++) {			
+			pixels3[i * 3 + k * target->pitch + 0] = pixels2[i * 3 + j * temp->pitch + 0];
+			pixels3[i * 3 + k * target->pitch + 1] = pixels2[i * 3 + j * temp->pitch + 1];
+			pixels3[i * 3 + k * target->pitch + 2] = pixels2[i * 3 + j * temp->pitch + 2]; 
+			k++;
+			}
+		}		
+	}
+	
+	SDL_Rect pos = {
+		(WINDOW_W - W2) / 2, (WINDOW_H - H2) / 2, W2, H2};
+	
+	SDL_BlitSurface(target, NULL, surface, &pos);
+	
+	SDL_UpdateWindowSurface(window);
+	bool shutdown = false;
+	while (!shutdown){
+	
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if ((event.type == SDL_KEYDOWN && event.key.keysym.sym ==
+				SDLK_ESCAPE) || event.type == SDL_QUIT) {
+					shutdown = true;
+					break;
+			}
+		}
+	}
+	SDL_FreeSurface(gradient);
+}
+
+/*---- Utilities ----*/
+
+// Prints the given QrCode object to the console.
+static void printQr(const QrCode &qr) {
+	int border = 4;
+	for (int y = -border; y < qr.getSize() + border; y++) {
+		for (int x = -border; x < qr.getSize() + border; x++) {
+			std::cout << (qr.getModule(x, y) ? "##" : "  ");
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+static void printQr1(const QrCode &qr) {
+	int border = 4;
+	for (int y = -border; y < qr.getSize() + border; y++) {
+		for (int x = -border; x < qr.getSize() + border; x++) {
+			std::cout << (qr.getModule(x, y) ? "[ 1 ]" : "[ 0 ]");
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+static void printQr2(const QrCode &qr) {
+	int border = 4;
+	 HDC hdc = GetDC(GetConsoleWindow());
+	for (int y = -border; y < qr.getSize() + border; y++) {
+		for (int x = -border; x < qr.getSize() + border; x++) {
+			if (!qr.getModule(x, y)) {
+			
+			SetPixel(hdc, x+ border, y + border, RGB(0, 0, 0));
+			}	else  {
+			SetPixel(hdc, x+ border, y+ border, RGB(255, 255, 255));
+			}
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
+static void printQr3(const QrCode &qr, const int border, SDL_Surface * target) {
+	
+	uint8_t *pixels = static_cast<uint8_t*>(target->pixels);
+
+	for (int y = -border; y < qr.getSize() + border; y++) {
+		for (int x = -border; x < qr.getSize() + border; x++) {
+			if (!qr.getModule(x,y)){
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 0] = 0xff;
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 1] = 0xff;
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 2] = 0xff; 
+			} else {
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 0] = 0x0;
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 1] = 0x0;
+				pixels[(x+4) * 3 + (y+4) * target->pitch + 2] = 0x0; 
+			}
+		}
+	}
+	return;
+}
